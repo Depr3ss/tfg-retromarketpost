54760b94a7ce54681098bf1d8f93534ef9d84fb2
Depr3ss <andresitomilea@gmail.com>
2025-06-07 19:29:20 +0200

    Arreglo ws

--- TFG_Proy/wsgi.py
+++ TFG_Proy/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TFG_Proy.settings')
 
 application = get_wsgi_application()
+
+# Ejecutar migraciones automáticamente al iniciar la app (útil en entornos sin shell)
+from django.core.management import call_command
+
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    # Puedes quitar o modificar el print para evitar que se muestre en producción
+    print(f"Error al ejecutar migraciones: {e}")
